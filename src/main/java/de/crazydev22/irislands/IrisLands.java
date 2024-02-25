b0f2ec5e2a89e68af90bf2465ee9ab0f5f321322
CrazyDev22 <juliankrings6@gmail.com>
2024-02-25 13:42:31 +0100

    save

--- src/main/java/de/crazydev22/irislands/IrisLands.java
+++ src/main/java/de/crazydev22/irislands/IrisLands.java
@@ -1,6 +1,5 @@
 package de.crazydev22.irislands;
 
-import com.fastasyncworldedit.core.function.block.SimpleBlockCopy;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
@@ -20,138 +19,169 @@ import net.jpountz.lz4.LZ4BlockInputStream;
 import net.jpountz.lz4.LZ4BlockOutputStream;
 import org.bukkit.Chunk;
 import org.bukkit.World;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.io.BukkitObjectInputStream;
-import org.bukkit.util.io.BukkitObjectOutputStream;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
 
 public final class IrisLands extends JavaPlugin implements Listener {
-    private final ExecutorService service = new ForkJoinPool();
-    private final Map<Long, Future<?>> futures = new HashMap<>();
-    private MantleWrapper wrapper;
+	private final ExecutorService service = new ForkJoinPool();
+	private final Map<Long, Future<?>> futures = new HashMap<>();
+	private MantleWrapper wrapper;
 
-    @Override
-    public void onEnable() {
-        try {
-            wrapper = new MantleWrapper();
-        } catch (NoSuchMethodException | NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-        getServer().getPluginManager().registerEvents(this, this);
-    }
+	@Override
+	public void onEnable() {
+		try {
+			wrapper = new MantleWrapper();
+		} catch (NoSuchMethodException | NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		}
+		getServer().getPluginManager().registerEvents(this, this);
+	}
 
-    @Override
-    public void onDisable() {
-        service.shutdown();
-    }
+	@Override
+	public void onDisable() {
+		service.shutdown();
+	}
 
-    @EventHandler
-    public void onPostClaim(ChunkPostClaimEvent event) {
-        var world = event.getWorld().getWorld();
-        var mantle = getMantle(world);
-        if (mantle == null) return;
-        Chunk chunk = world.getChunkAt(event.getX(), event.getZ());
-        submit(chunk, () -> {
-            chunk.addPluginChunkTicket(this);
+	@EventHandler
+	public void onChunkLoad(ChunkLoadEvent event) {
+		save(event.getWorld(), event.getChunk().getX(), event.getChunk().getZ(), false);
+	}
 
-            try {
-                try (var out = new LZ4BlockOutputStream(new FileOutputStream(getFile(world, true, event.getX(), event.getZ())))) {
-                    wrapper.write(wrapper.getChunk(mantle, event.getX(), event.getZ()), out);
-                }
-                try (var out = new FileOutputStream(getFile(world, false, event.getX(), event.getZ()));
-                     var editSession = WorldEdit.getInstance().newEditSession(new BukkitWorld(world))) {
-                    var region = new CuboidRegion(new BukkitWorld(world),
-                            BlockVector3.at(event.getX() << 4, world.getMinHeight(), event.getZ() << 4),
-                            BlockVector3.at((event.getX() << 4) + 15, world.getMaxHeight(), (event.getZ() << 4) + 15));
-                    try (var clipboard = new BlockArrayClipboard(region)) {
-                        var copy = new ForwardExtentCopy(editSession, region, clipboard, region.getMinimumPoint());
-                        Operations.complete(copy);
-                        clipboard.save(out, BuiltInClipboardFormat.FAST);
-                    }
-                }
-            } catch (Throwable e) {
-                throw new RuntimeException(e);
-            } finally {
-                chunk.removePluginChunkTicket(this);
-            }
-        });
-    }
+	@EventHandler
+	public void onPostClaim(ChunkPostClaimEvent event) {
+		save(event.getWorld().getWorld(), event.getX(), event.getZ(), false);
+	}
 
-    @EventHandler
-    public void onChunkDelete(ChunkDeleteEvent event) {
-        var world = event.getWorld();
-        var mantle = getMantle(world);
-        if (mantle == null) return;
-        Chunk chunk = world.getChunkAt(event.getX(), event.getZ());
-        submit(chunk, () -> {
-            chunk.addPluginChunkTicket(this);
+	@EventHandler
+	public void onChunkDelete(ChunkDeleteEvent event) {
+		load(event.getWorld(), event.getX(), event.getZ(), false);
+	}
 
-            try {
-                var mantleFile = getFile(world, true, event.getX(), event.getZ());
-                try (var in = new LZ4BlockInputStream(new FileInputStream(mantleFile))) {
-                    wrapper.setChunk(mantle, event.getX(), event.getZ(), wrapper.read(mantle, in));
-                } finally {
-                    mantleFile.delete();
-                }
+	@ChunkCoordinates
+	public void load(World world, int x, int z, boolean delete) {
+		Chunk chunk = world.getChunkAt(x, z);
+		submit(chunk, () -> {
+			chunk.addPluginChunkTicket(this);
 
-                var chunkFile = getFile(world, false, event.getX(), event.getZ());
-                try (var reader = BuiltInClipboardFormat.FAST.getReader(new FileInputStream(chunkFile));
-                     var editSession = WorldEdit.getInstance().newEditSession(new BukkitWorld(world))) {
-                    var operation = new ClipboardHolder(reader.read())
-                            .createPaste(editSession)
-                            .to(BlockVector3.at(event.getX() << 4, world.getMinHeight(), event.getZ() << 4))
-                            .ignoreAirBlocks(false)
-                            .build();
-                    Operations.complete(operation);
-                } finally {
-                    chunkFile.delete();
-                }
-            } catch (Throwable e) {
-                throw new RuntimeException(e);
-            } finally {
-                chunk.removePluginChunkTicket(this);
-            }
-        });
-    }
+			try {
+				var mantle = getMantle(world);
+				if (mantle != null) {
+					var mantleFile = getFile(world, true, x, z);
+					try (var in = new LZ4BlockInputStream(new FileInputStream(mantleFile))) {
+						wrapper.setChunk(mantle, x, z, wrapper.read(mantle, in));
+					} catch (Throwable e) {
+						getLogger().log(Level.WARNING, "Failed to load mantle chunk: " + x + ", " + z, e);
+					} finally {
+						if (delete && !mantleFile.delete()) {
+							getLogger().warning("Failed to delete mantle chunk: " + x + ", " + z);
+						}
+					}
+				}
 
-    private void submit(Chunk chunk, Runnable runnable) {
-        long chunkId = Cache.key(chunk);
-        var future = futures.remove(chunkId);
-        if (future != null) {
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-        future = service.submit(runnable);
-        futures.put(chunkId, future);
-    }
+				var chunkFile = getFile(world, false, x, z);
+				try (var reader = BuiltInClipboardFormat.FAST.getReader(new FileInputStream(chunkFile));
+					 var editSession = WorldEdit.getInstance().newEditSession(new BukkitWorld(world))) {
+					var operation = new ClipboardHolder(reader.read())
+							.createPaste(editSession)
+							.to(BlockVector3.at(x << 4, world.getMinHeight(), z << 4))
+							.ignoreAirBlocks(false)
+							.build();
+					Operations.complete(operation);
+				} catch (Throwable e) {
+					getLogger().log(Level.WARNING, "Failed to load world chunk: " + x + ", " + z, e);
+				} finally {
+					if (delete && !chunkFile.delete()) {
+						getLogger().warning("Failed to delete world chunk: " + x + ", " + z);
+					}
+				}
+			} catch (Throwable e) {
+				throw new RuntimeException(e);
+			} finally {
+				chunk.removePluginChunkTicket(this);
+			}
+		});
+	}
 
-    @ChunkCoordinates
-    private File getFile(World world, boolean mantle, int x, int z) {
-        File file = new File(getDataFolder(), world.getName() + File.separator + x + "_" + z + (mantle ? ".lz4b" : ".schem"));
-        if (!file.getParentFile().exists())
-            file.getParentFile().mkdirs();
-        return file;
-    }
+	@ChunkCoordinates
+	public void save(World world, int x, int z, boolean overwrite) {
+		Chunk chunk = world.getChunkAt(x, z);
+		submit(chunk, () -> {
+			chunk.addPluginChunkTicket(this);
+
+			try {
+				var mantle = getMantle(world);
+				if (mantle != null) {
+					if (!getFile(world, true, x, z).exists() || overwrite) {
+						try (var out = new LZ4BlockOutputStream(new FileOutputStream(getFile(world, true, x, z)))) {
+							wrapper.write(wrapper.getChunk(mantle, x, z), out);
+						} catch (Throwable e) {
+							getLogger().log(Level.SEVERE, "Failed to save mantle chunk: " + x + ", " + z, e);
+						}
+					}
+				}
+
+				if (!getFile(world, false, x, z).exists() || overwrite) {
+					try (var out = new FileOutputStream(getFile(world, false, x, z));
+						 var editSession = WorldEdit.getInstance().newEditSession(new BukkitWorld(world))) {
+						var region = new CuboidRegion(new BukkitWorld(world),
+								BlockVector3.at(x << 4, world.getMinHeight(), z << 4),
+								BlockVector3.at((x << 4) + 15, world.getMaxHeight(), (z << 4) + 15));
+						try (var clipboard = new BlockArrayClipboard(region)) {
+							var copy = new ForwardExtentCopy(editSession, region, clipboard, region.getMinimumPoint());
+							Operations.complete(copy);
+							clipboard.save(out, BuiltInClipboardFormat.FAST);
+						}
+					} catch (Throwable e) {
+						getLogger().log(Level.SEVERE, "Failed to save world chunk: " + x + ", " + z, e);
+					}
+				}
+			} finally {
+				chunk.removePluginChunkTicket(this);
+			}
+		});
+	}
+
+	private void submit(Chunk chunk, Runnable runnable) {
+		long chunkId = Cache.key(chunk);
+		var future = futures.remove(chunkId);
+		if (future != null) {
+			try {
+				future.get();
+			} catch (InterruptedException | ExecutionException e) {
+                getLogger().log(Level.WARNING, "Failed chunk action on chunk " + chunk, e);
+			}
+		}
+		future = service.submit(runnable);
+		futures.put(chunkId, future);
+	}
+
+	@ChunkCoordinates
+	private File getFile(World world, boolean mantle, int x, int z) {
+		File file = new File(getDataFolder(), world.getName() + File.separator + x + "_" + z + (mantle ? ".lz4b" : ".schem"));
+		if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
+            throw new RuntimeException("Failed to create directory: " + file.getParentFile());
+		return file;
+	}
 
 
-    private Mantle getMantle(World world) {
-        var platform = IrisToolbelt.access(world);
-        if (platform == null)
-            return null;
-        var engine = platform.getEngine();
-        return engine != null ? engine.getMantle().getMantle() : null;
-    }
+	private Mantle getMantle(World world) {
+		var platform = IrisToolbelt.access(world);
+		if (platform == null)
+			return null;
+		var engine = platform.getEngine();
+		return engine != null ? engine.getMantle().getMantle() : null;
+	}
 }
--- src/main/java/de/crazydev22/irislands/MantleWrapper.java
+++ src/main/java/de/crazydev22/irislands/MantleWrapper.java
@@ -5,8 +5,6 @@ import com.volmit.iris.util.documentation.ChunkCoordinates;
 import com.volmit.iris.util.mantle.Mantle;
 import com.volmit.iris.util.mantle.MantleChunk;
 import com.volmit.iris.util.mantle.TectonicPlate;
-import net.jpountz.lz4.LZ4BlockInputStream;
-import net.jpountz.lz4.LZ4BlockOutputStream;
 
 import java.io.*;
 import java.lang.reflect.Field;
@@ -16,50 +14,50 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 
 @SuppressWarnings("unchecked")
 public class MantleWrapper {
-    private final Method getPlate;
-    private final Field chunks;
+	private final Method getPlate;
+	private final Field chunks;
 
-    public MantleWrapper() throws NoSuchMethodException, NoSuchFieldException {
-        getPlate = Mantle.class.getDeclaredMethod("get", int.class, int.class);
-        getPlate.setAccessible(true);
+	public MantleWrapper() throws NoSuchMethodException, NoSuchFieldException {
+		getPlate = Mantle.class.getDeclaredMethod("get", int.class, int.class);
+		getPlate.setAccessible(true);
 
-        chunks = TectonicPlate.class.getDeclaredField("chunks");
-        chunks.setAccessible(true);
-    }
+		chunks = TectonicPlate.class.getDeclaredField("chunks");
+		chunks.setAccessible(true);
+	}
 
-    public MantleChunk read(Mantle mantle, InputStream in) {
-        try {
-            return new MantleChunk(mantle.getWorldHeight() >> 4, new DataInputStream(in));
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public MantleChunk read(Mantle mantle, InputStream in) {
+		try {
+			return new MantleChunk(mantle.getWorldHeight() >> 4, new DataInputStream(in));
+		} catch (IOException | ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public void write(MantleChunk chunk, OutputStream out) {
-        try {
-            chunk.write(new DataOutputStream(out));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public void write(MantleChunk chunk, OutputStream out) {
+		try {
+			chunk.write(new DataOutputStream(out));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @ChunkCoordinates
-    public MantleChunk getChunk(Mantle mantle, int x, int z) {
-        try {
-            return ((TectonicPlate) getPlate.invoke(mantle, x >> 5, z>> 5))
-                    .getOrCreate(x & 31, z & 31);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@ChunkCoordinates
+	public MantleChunk getChunk(Mantle mantle, int x, int z) {
+		try {
+			return ((TectonicPlate) getPlate.invoke(mantle, x >> 5, z >> 5))
+					.getOrCreate(x & 31, z & 31);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @ChunkCoordinates
-    public void setChunk(Mantle mantle, int x, int z, MantleChunk chunk) {
-        try {
-            var array = (AtomicReferenceArray<MantleChunk>) chunks.get(getPlate.invoke(mantle, x >> 4 >> 5, z >> 4 >> 5));
-            array.set(Cache.to1D(x >> 4 & 31, z >> 4 & 31, 0, 32, 32), chunk);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@ChunkCoordinates
+	public void setChunk(Mantle mantle, int x, int z, MantleChunk chunk) {
+		try {
+			var array = (AtomicReferenceArray<MantleChunk>) chunks.get(getPlate.invoke(mantle, x >> 4 >> 5, z >> 4 >> 5));
+			array.set(Cache.to1D(x >> 4 & 31, z >> 4 & 31, 0, 32, 32), chunk);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
